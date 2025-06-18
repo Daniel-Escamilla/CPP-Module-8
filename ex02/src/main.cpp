c98e1c748d3a279ff99568cce74b4a25b4a9e825
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-06-18 18:43:40 +0200

    feat: add main function to demonstrate MutantStack functionality

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -3,13 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: daniel-escamilla <daniel-escamilla@stud    +#+  +:+       +#+        */
+/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 12:37:20 by daniel-esca       #+#    #+#             */
-/*   Updated: 2025/06/18 12:37:21 by daniel-esca      ###   ########.fr       */
+/*   Updated: 2025/06/18 18:21:35 by descamil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/MutantStack.hpp"
 
+int main()
+{
+    MutantStack<int, std::vector<int> > stack;
+    stack.push(10);
+    stack.push(20);
+    stack.push(30);
 
+    std::cout << "Top: " << stack.top() << std::endl;
+
+    std::cout << "Elements: ";
+    for (MutantStack<int, std::vector<int> >::iterator it = stack.begin(); it != stack.end(); ++it)
+        std::cout << *it << " ";
+    std::cout << std::endl;
+
+    stack.pop();
+    std::cout << "After pop, top: " << stack.top() << std::endl;
+    std::cout << "Size: " << stack.size() << std::endl;
+
+    MutantStack<int, std::vector<int> > copyStack(stack);
+    MutantStack<int, std::vector<int> > assignedStack;
+    assignedStack = stack;
+
+    std::cout << "CopyStack elements: ";
+    for (MutantStack<int, std::vector<int> >::iterator it = copyStack.begin(); it != copyStack.end(); ++it)
+        std::cout << *it << " ";
+    std::cout << std::endl;
+
+    std::cout << "AssignedStack elements: ";
+    for (MutantStack<int, std::vector<int> >::iterator it = assignedStack.begin(); it != assignedStack.end(); ++it)
+        std::cout << *it << " ";
+    std::cout << std::endl;
+
+    return 0;
+}

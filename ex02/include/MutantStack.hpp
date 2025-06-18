8a11db801bca8b3566f4fcf6d317288a6e62d38d
Daniel-Escamilla <Daniel.escamilla.bq@gmail.com>
2025-06-18 16:16:27 +0200

    Add MutantStack class definition with basic constructors

---
+++ ex02/include/MutantStack.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daniel-escamilla <daniel-escamilla@stud    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/18 12:37:23 by daniel-esca       #+#    #+#             */
+/*   Updated: 2025/06/18 16:15:45 by daniel-esca      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <iostream>
+# include <string>
+# include <deque>
+
+class MutantStack
+{
+	private:
+		
+	public:
+		MutantStack();
+		~MutantStack();
+		MutantStack(const MutantStack& copy);
+		
+};
+
+#endif // MUTANTSTACK_HPP
